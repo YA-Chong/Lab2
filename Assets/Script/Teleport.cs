157491c565cdce74a8c545e4e592e0585d3619c5
Hp1ayer <fallenstar1213@gmail.com>
2026-03-01 14:25:33 +1100

    Debug Map V 0.8
    Change Camera limit function
    Change physic Material

--- Assets/Script/Teleport.cs
+++ Assets/Script/Teleport.cs
@@ -1,15 +1,23 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
     public Transform targetPoint;
+    public CinemachineCamera virtualCamera; 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Please note that this code assumes the player has a tag "Player" assigned to it in the Unity Editor.
         if (other.CompareTag("Mario"))
         {
+            // 计算位移差值
+            Vector3 delta = targetPoint.position - other.transform.position;
+
+            // 玩家瞬移
             other.transform.position = targetPoint.position;
+
+            // 通知Cinemachine这是一次瞬移
+            virtualCamera.OnTargetObjectWarped(other.transform, delta);
         }
     }
 }
